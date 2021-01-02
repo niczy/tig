73aca1150acf8364007c84c8397cf1ea428380b2
Nicholas <nicholas.zhao@bytedance.com>
2021-01-01 22:00:07 -0800

    cmd line.

--- server.go
+++ server.go
@@ -1,16 +1,31 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"os"
 )
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	server := flag.Bool("server", false, "Whether this is Tig server.")
+	flag.Parse()
+	if *server {
+		r := gin.Default()
+		r.GET("/ping", func(c *gin.Context) {
 
-		c.JSON(200, gin.H{
-			"message": "pong",
+			c.JSON(200, gin.H{
+				"message": "pong",
+			})
 		})
-	})
-	r.Run()
+		r.Run()
+	} else {
+		subCmd := os.Args[1]
+		switch subCmd {
+		case "submit":
+			fmt.Println("submit command")
+		case "export":
+			fmt.Println("export command")
+		}
+	}
 }
